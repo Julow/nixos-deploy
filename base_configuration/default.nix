3308ba3d188830587679f10c5a6c3d9f0f27ea39
Jules Aguillon <juloo.dsi@gmail.com>
2021-12-24 00:14:23 +0100

    Write the activation script in Nix

--- base_configuration/default.nix
+++ base_configuration/default.nix
@@ -1,3 +1,22 @@
-{ configuration, nixos ? import <nixpkgs/nixos> }:
+{ configuration, system ? builtins.currentSystem }:
 
-nixos { configuration.imports = [ configuration ./check_hostname.nix ]; }
+let
+  eval = extra_modules:
+    import <nixpkgs/nixos/lib/eval-config.nix> {
+      inherit system;
+      modules = [ configuration ./check_hostname.nix ] ++ extra_modules;
+    };
+
+in {
+  vm = (eval [
+    <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+  ]).config.system.build.vm;
+
+  system = let
+    inherit (eval [ ]) config pkgs;
+    inherit (config.system.build) toplevel;
+  in pkgs.writeShellScriptBin "activate" ''
+    nix-env --profile /nix/var/nix/profiles/system --set '${toplevel}' &&
+    /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+  '';
+}
