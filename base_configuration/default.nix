22d90b722f19a241b83ed410a8cfa80ffad01e2a
Jules Aguillon <juloo.dsi@gmail.com>
2021-12-24 00:45:44 +0100

    Check that the hostname won't change after a deploy

--- base_configuration/default.nix
+++ base_configuration/default.nix
@@ -16,6 +16,15 @@ in {
     inherit (eval [ ]) config pkgs;
     inherit (config.system.build) toplevel;
   in pkgs.writeShellScriptBin "activate" ''
+    current_hostname=`cat /etc/hostname`
+    new_hostname='${config.networking.hostName}'
+    if [[ $current_hostname != $new_hostname ]]; then
+      echo "Warning: Deploying a system with hostname '$new_hostname' on a machine with hostname '$current_hostname'"
+      if [[ $NO_CHECK_HOSTNAME -ne 1 ]]; then
+        echo "Error: Set the environment variable 'NO_CHECK_HOSTNAME=1' to confirm."
+        exit 1
+      fi
+    fi
     nix-env --profile /nix/var/nix/profiles/system --set '${toplevel}' &&
     /nix/var/nix/profiles/system/bin/switch-to-configuration switch
   '';
